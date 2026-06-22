3b834715dd5d0b7b5399a7499e148948cde7183f
Daniel <daniel.figueroa.14@sansano.usm.cl>
2026-06-22 12:00:16 -0400

    Precompute metrics

--- main_app.py
+++ main_app.py
@@ -5,7 +5,7 @@ from layout import app_layout
 from precompute_metrics import precompute_file
 
 app = dash.Dash(__name__, title="UHF Pollution Flashover Monitor")
-archivo_hdf5 = r"E:\Chunks\data\N12_25KV.hdf5"
+archivo_hdf5 = r"E:\Chunks\data\nuevo_exp_ciclos_12.hdf5"
 
 app_layout(app)
 app_callbacks(app, archivo_hdf5)
